3cea9344453e02e8389ef9f419a2c3c750fbfe16
electrohyun <dev.electrohyun@gmail.com>
2025-06-30 17:35:25 +0900

    [WIP] 프론트 수정 구현, console error fix 필요

--- main.js
+++ main.js
@@ -42,9 +42,7 @@ function addTodo() {
     editImg.setAttribute("alt", "수정");
     editBtn.appendChild(editImg);
     editBtn.classList.add("todo-optBtn");
-    // editBtn.addEventListener("click", () => {
-
-    // })
+    editBtn.addEventListener("click", () => editTodo(span, editBtn, div));
 
     const deleteBtn = document.createElement("button");
     const deleteImg = document.createElement("img");
@@ -60,9 +58,50 @@ function addTodo() {
     div.appendChild(span);
     div.appendChild(editBtn);
     div.appendChild(deleteBtn);
-    
 
     div.classList.add("todo-list");
     list.appendChild(div);
     input.value = "";
+}
+
+function editTodo(span, edit, div) {
+    // sp가 input으로 바뀜
+    const inputField = document.createElement("input");
+    inputField.value = span.textContent;
+    inputField.classList.add("todo-span");
+    div.replaceChild(inputField, span);
+    inputField.focus();
+
+    // 수정버튼이 완료버튼으로 바뀜
+    const doneBtn = document.createElement("button");
+    doneBtn.textContent = "완료";
+    doneBtn.classList.add("todo-completeBtn");
+    div.replaceChild(doneBtn, edit);
+
+    doneBtn.addEventListener("click", () => {
+        if (inputField.value.trim() === "") {
+            alert("빈 값 저장 인됩니다.");
+            return;
+        }
+
+        const newSpan = document.createElement("span");
+        newSpan.textContent = inputField.value;
+        newSpan.classList.add("todo-span");
+        div.replaceChild(newSpan, inputField);
+
+        const newEditBtn = document.createElement("button");
+        const newEditImg = document.createElement("img");
+        newEditImg.setAttribute("src", "Edit.svg");
+        newEditImg.setAttribute("alt", "수정");
+        newEditBtn.appendChild(newEditImg);
+        newEditBtn.classList.add("todo-optBtn");
+        div.replaceChild(edit, doneBtn);
+
+        edit.addEventListener("click", () => {
+            editTodo(newSpan, newEditBtn, div);
+        });
+
+
+    });
+
 }
