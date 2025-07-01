191bfc6384212e14d54e2517fe487d7b7c12c994
electrohyun <dev.electrohyun@gmail.com>
2025-07-01 11:38:33 +0900

    프론트 구현 완료

--- main.js
+++ main.js
@@ -42,7 +42,12 @@ function addTodo() {
     editImg.setAttribute("alt", "수정");
     editBtn.appendChild(editImg);
     editBtn.classList.add("todo-optBtn");
-    editBtn.addEventListener("click", () => editTodo(span, editBtn, div));
+
+    function handler() {
+        editTodo(span, editBtn, handler, div);
+    }
+
+    editBtn.addEventListener("click", handler, { once : true });
 
     const deleteBtn = document.createElement("button");
     const deleteImg = document.createElement("img");
@@ -64,7 +69,7 @@ function addTodo() {
     input.value = "";
 }
 
-function editTodo(span, edit, div) {
+function editTodo(span, edit, handler, div) {
     // sp가 input으로 바뀜
     const inputField = document.createElement("input");
     inputField.value = span.textContent;
@@ -73,35 +78,33 @@ function editTodo(span, edit, div) {
     inputField.focus();
 
     // 수정버튼이 완료버튼으로 바뀜
-    const doneBtn = document.createElement("button");
-    doneBtn.textContent = "완료";
-    doneBtn.classList.add("todo-completeBtn");
-    div.replaceChild(doneBtn, edit);
+    // 수정버튼의 text = "완료", css 변경, 기능 변경
+    edit.innerText = "완료";
+    edit.classList.replace("todo-optBtn", "todo-completeBtn");
+    // 기존 기능 삭제 후 새 기능 부착
+    // 새 기능: 완료를 누르면
+    // 1. 빈값 경고
+    // 2. input -> span
+    // 3. edit 버튼 수정 (텍스트 -> 그림)
+    edit.removeEventListener("click", handler);
 
-    doneBtn.addEventListener("click", () => {
+    edit.addEventListener("click", () => {
         if (inputField.value.trim() === "") {
-            alert("빈 값 저장 인됩니다.");
+            alert("빈 값 저장 안됩니다.");
             return;
         }
 
-        const newSpan = document.createElement("span");
-        newSpan.textContent = inputField.value;
-        newSpan.classList.add("todo-span");
-        div.replaceChild(newSpan, inputField);
+        // input -> span 교체
+        span.textContent = inputField.value;
+        if (div.contains(inputField)) div.replaceChild(span, inputField);
+        // 버튼 교체
+        edit.innerText = "";
+        const editImg = document.createElement("img");
+        editImg.setAttribute("src", "Edit.svg");
+        editImg.setAttribute("alt", "수정");
+        edit.appendChild(editImg);
+        edit.classList.replace("todo-completeBtn", "todo-optBtn");
 
-        const newEditBtn = document.createElement("button");
-        const newEditImg = document.createElement("img");
-        newEditImg.setAttribute("src", "Edit.svg");
-        newEditImg.setAttribute("alt", "수정");
-        newEditBtn.appendChild(newEditImg);
-        newEditBtn.classList.add("todo-optBtn");
-        div.replaceChild(edit, doneBtn);
-
-        edit.addEventListener("click", () => {
-            editTodo(newSpan, newEditBtn, div);
-        });
-
-
+        edit.addEventListener("click", handler, { once : true });
     });
-
 }
